a8b4896592768ede63beee2c75f445232a9f7df8
SokolAlexander <asokol1994@gmail.com>
2022-01-28 09:05:57 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Message } from "./components/Message";
 import { Counter } from "./components/Counter";
-
-const myText = "Hello from app!!!";
+import { Form } from "./components/Form";
+import { useEffect } from "react/cjs/react.development";
 
 function App() {
+  const [messageList, setMessageList] = useState([
+    { text: "msg1", author: "Me" },
+    { text: "i am bot", author: "Robot" },
+  ]);
   const handleMessageClick = () => {
     console.log("hello!!!");
   };
 
+  const handleAddMessage = (text) => {
+    setMessageList((prevMessageList) => [...prevMessageList, text]);
+  };
+
+  useEffect(() => {
+    console.log(messageList[messageList.length - 1]);
+  }, [messageList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Message
-          myString="my string"
-          text={myText}
-          onMessageClick={handleMessageClick}
-        />
-        <Counter />
-      </header>
-    </div>
+    <>
+      <h2>FRAGMENT</h2>
+      <div className="App">
+        <header className="App-header">
+          {messageList.map((text) => (
+            <Message text={text} onMessageClick={handleMessageClick} />
+          ))}
+          {/* <Counter /> */}
+          <Form onSubmit={handleAddMessage} />
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -10,10 +10,6 @@ import "./styles.css";
 export class Message extends React.Component {
   render() {
     const { text, onMessageClick } = this.props;
-    return (
-      <h3 className="header" onClick={onMessageClick}>
-        Message Text, {text}
-      </h3>
-    );
+    return <span onClick={onMessageClick}>Message Text, {text}</span>;
   }
 }
